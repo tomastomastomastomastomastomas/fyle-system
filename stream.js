174382c26ed7e798c97c86ca8dd8683aab658916
tomas <tomas.gentile@gm2dev.com>
2024-10-22 12:08:25 -0300

    feat(stream): add manage errors in a file

--- stream.js
+++ stream.js
@@ -7,10 +7,21 @@ readStream.on("data", (chunk) => {
   console.log("Pedazo de texto: ", chunk);
 });
 readStream.on("error", (err) => {
-  console.log(err);
+  appendError(err);
 });
 writeStream.on("error", (err) => {
-  console.log(err);
+  appendError(err);
 });
+
+function appendError(err = null) {
+  if (err) {
+    fs.appendFile("errors.txt", `\n${err}`, (appendErr) => {
+      if (appendErr) {
+        console.log(appendErr);
+        return;
+      }
+    });
+  }
+}
 
 readStream.pipe(writeStream);
