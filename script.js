6f6761a458a9eb0b2a1fdf7f072551fc596390c1
tomas <tomas.gentile@gm2dev.com>
2024-10-22 10:54:41 -0300

    feat(fyleSystemExercise): add async method

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
 const fs = require("fs");
-try {
-  console.log(fs.readFileSync("data.txt", "utf8"));
-} catch (err) {
-  console.log("hubo un error al leer el archivo : ", err);
-  try {
-    fs.writeFileSync("data.txt", "hola");
-  } catch (err) {
-    console.log("hubo un erro al crear el archivo: ", err);
+// try {
+//   console.log(fs.readFileSync("data.txt", "utf8"));
+// } catch (err) {
+//   console.log("hubo un error al leer el archivo : ", err);
+//   try {
+//     fs.writeFileSync("data.txt", "hola");
+//   } catch (err) {
+//     console.log("hubo un erro al crear el archivo: ", err);
+//   }
+// }
+
+// try {
+//   fs.appendFileSync("data.txt", "\n hola diferente al primer hola");
+// } catch (err) {
+//   console.log("hubo un error al modificar el archivo : ", err);
+// }
+
+// try {
+//   console.log(fs.readFileSync("data.txt", "utf8"));
+// } catch (err) {
+//   console.log("hubo un error al leer el archivo : ", err);
+// }
+
+// try {
+//   fs.unlinkSync("data.txt");
+// } catch (err) {
+//   console.log("hubo un error al eliminar el archivo : ", err);
+// }
+
+// try {
+//   fs.readFileSync("realData.txt", "utf-8");
+// } catch (err) {
+//   fs.appendFileSync("log.txt", `\n${err}`);
+// }
+
+fs.readFile("data.txt", "utf-8", (err, data) => {
+  if (err) {
+    fs.writeFile("data.txt", "hola", (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
   }
-}
-
-try {
-  fs.appendFileSync("data.txt", "\n hola diferente al primer hola");
-} catch (err) {
-  console.log("hubo un error al modificar el archivo : ", err);
-}
-
-try {
-  console.log(fs.readFileSync("data.txt", "utf8"));
-} catch (err) {
-  console.log("hubo un error al leer el archivo : ", err);
-}
-
-try {
-  fs.unlinkSync("data.txt");
-} catch (err) {
-  console.log("hubo un error al eliminar el archivo : ", err);
-}
-
-try {
-  fs.readFileSync("realData.txt", "utf-8");
-} catch (err) {
-  fs.appendFileSync("log.txt", `\n${err}`);
-}
+  console.log(data);
+  fs.appendFile("data.txt", "\n hola diferente al primer hola", (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    fs.readFile("data.txt", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(data);
+      fs.unlink("data.txt", (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        fs.readFile("realData.txt", "utf-8", (err) => {
+          if (err) {
+            console.log(err);
+            fs.appendFileSync("log.txt", `\n${err}`);
+          }
+        });
+      });
+    });
+  });
+});
